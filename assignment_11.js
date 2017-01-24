82f7601885762108f780621b7eec19dedfa73b47
Christopher Dougherty <christophertdoc@gmail>
2017-01-24 14:11:24 -0500

    first file

---
+++ assignment_11.js
@@ -0,0 +1,8 @@
+var myName = "Chris",
+    courseTrack = "Frontend Web Development",
+    courseType = "18 week";
+
+console.log("My name is " + myName + ", and I'm taking Bloc's " +
+           courseTrack + " course on the " + courseType + " track");
+
+    
